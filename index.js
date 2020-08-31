3c168f3f2651f2fef9a1c98edf4fe53f2913d4d1
elandTong <eland.tong@support888.net>
2020-08-31 17:23:15 +0700

    update index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Eland.Tong
  * @Date: 2020-05-19 15:40:02
- * @LastEditTime: 2020-08-11 09:19:02
+ * @LastEditTime: 2020-08-31 17:22:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /ReactElandFramework/index.js
@@ -11,28 +11,30 @@ import Config from './src/Config'
 import BaseModal from './src/router/BaseModal'
 import BaseScreen from './src/router/BaseScreen'
 import Frame from './src/router/Frame'
-import ModalFrame from './src/router/ModalFrame'
 import { ModalPage, ScreenPage } from './src/router/Page'
-import ScreenFrame from './src/router/ScreenFrame'
 import Emit from './src/utils/EventBus'
 import NetApi from './src/utils/NetApi'
 import Tool from './src/utils/Tool'
 import Banner from './src/widget/Banner'
 import Button from './src/widget/Button'
+import DrawerMenu from './src/widget/DrawerMenu'
 import FixedModal from './src/widget/FixedModal'
+import FixedModalGroup from './src/widget/FixedModalGroup'
+import GestureLock from './src/widget/GestureLock'
 import ListButton from './src/widget/ListButton'
 import MarqueeBar from './src/widget/MarqueeBar'
 import Mask from './src/widget/Mask'
 import MessageNotice from './src/widget/MessageNotice'
 import Minirefresh from './src/widget/Minirefresh'
 import Navbar from './src/widget/Navbar'
+import { NumberKeyboardController } from './src/widget/NumberKeyboard'
 import Preblock from './src/widget/Preblock'
 import SearchBar from './src/widget/SearchBar'
 import Sorry from './src/widget/Sorry'
 import { TabSlide, TabSwiper } from './src/widget/Swiper'
 import ToolbarMenu from './src/widget/ToolbarMenu'
 
-export { Frame, ScreenFrame, BaseScreen, ModalFrame, BaseModal, ScreenPage, ModalPage }
-export { Banner, Button, ListButton, Minirefresh, MarqueeBar, MessageNotice, FixedModal, Mask, Navbar, Preblock, SearchBar, Sorry, TabSwiper, TabSlide, ToolbarMenu }
+export { Frame, BaseScreen, BaseModal, ScreenPage, ModalPage }
+export { Banner, Button, ListButton, Minirefresh, MarqueeBar, MessageNotice, FixedModal, FixedModalGroup, Mask, Navbar, Preblock, SearchBar, Sorry, TabSwiper, TabSlide, ToolbarMenu, NumberKeyboardController, DrawerMenu, GestureLock }
 export { Tool, Emit, NetApi }
 export { Config }
