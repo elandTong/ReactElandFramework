57ac1774a1497b1762d0e499961cc7ba7f0ce314
elandTong <eland.tong@support888.net>
2020-09-15 17:30:27 +0700

    update root index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Eland.Tong
  * @Date: 2020-05-19 15:40:02
- * @LastEditTime: 2020-08-31 17:42:47
+ * @LastEditTime: 2020-09-15 17:29:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /ReactElandFramework/index.js
@@ -12,12 +12,15 @@ import BaseModal from './src/router/BaseModal'
 import BaseScreen from './src/router/BaseScreen'
 import Frame from './src/router/Frame'
 import { ModalPage, ScreenPage } from './src/router/Page'
+import Business from './src/utils/Business'
 import Emit from './src/utils/EventBus'
 import NetApi from './src/utils/NetApi'
-import Tool from './src/utils/Tool'
+import ResUtil from './src/utils/ResUtil'
+import Tool, { ModalTool, RouterTool } from './src/utils/Tool'
 import Banner from './src/widget/Banner'
 import Button from './src/widget/Button'
 import DrawerMenu from './src/widget/DrawerMenu'
+import DrawerMenuContent, { DrawerMenuBottom, DrawerMenuHeader, DrawerMenuItem } from './src/widget/DrawerMenuContent'
 import FixedModal from './src/widget/FixedModal'
 import FixedModalGroup from './src/widget/FixedModalGroup'
 import GestureLock from './src/widget/GestureLock'
@@ -30,12 +33,41 @@ import Navbar from './src/widget/Navbar'
 import { NumberKeyboardController } from './src/widget/NumberKeyboard'
 import Preblock from './src/widget/Preblock'
 import SearchBar from './src/widget/SearchBar'
+import SelectPanel from './src/widget/SelectPanel'
 import Sorry from './src/widget/Sorry'
 import { TabSlide, TabSwiper } from './src/widget/Swiper'
 import ToolbarMenu from './src/widget/ToolbarMenu'
 import TouchEffect from './src/widget/TouchEffect'
+import VirtualBufferList from './src/widget/VirtualBufferList'
 
 export { Frame, BaseScreen, BaseModal, ScreenPage, ModalPage }
-export { Banner, Button, ListButton, Minirefresh, MarqueeBar, MessageNotice, FixedModal, FixedModalGroup, Mask, Navbar, Preblock, SearchBar, Sorry, TabSwiper, TabSlide, ToolbarMenu, NumberKeyboardController, DrawerMenu, GestureLock, TouchEffect }
-export { Tool, Emit, NetApi }
+export {
+    Banner,
+    Button,
+    ListButton,
+    Minirefresh,
+    MarqueeBar,
+    MessageNotice,
+    FixedModal,
+    FixedModalGroup,
+    Mask,
+    Navbar,
+    Preblock,
+    SearchBar,
+    Sorry,
+    TabSwiper,
+    TabSlide,
+    ToolbarMenu,
+    NumberKeyboardController,
+    DrawerMenu,
+    GestureLock,
+    TouchEffect,
+    DrawerMenuContent,
+    DrawerMenuHeader,
+    DrawerMenuBottom,
+    DrawerMenuItem,
+    VirtualBufferList,
+    SelectPanel
+}
+export { Tool, RouterTool, ModalTool, Business, ResUtil, Emit, NetApi }
 export { Config }
